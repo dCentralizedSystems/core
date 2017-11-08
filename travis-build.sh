3efa4e1da97f7898e61e6a238b16813af57d2e5e
georgios chrysanthakopoulos <georgechrysanthakopoulos@live.com>
2017-11-08 09:47:12 -0800

    update script

--- travis-build.sh
+++ travis-build.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 # Build script for Travis CI.
 
-export WORKING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export OUTPUT_FILE=$WORKING_DIR/output.out
-touch $OUTPUT_FILE
-
 set -ex
 
-mvn install -DskipTests=true >> $OUTPUT_FILE 2>&1
+mvn install -DskipTests=true
 
