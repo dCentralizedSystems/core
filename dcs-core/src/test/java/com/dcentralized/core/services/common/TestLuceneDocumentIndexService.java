31d0edb70a03b178a98817146ad9975fb1516243
georgios chrysanthakopoulos <georgechrysanthakopoulos@live.com>
2018-02-07 14:50:24 -0800

    apply xenon a1b5add0b86f5daf6d24682274bf4cd617ec2334
    https://github.com/vmware/xenon/commit/a1b5add0b86f5daf6d24682274bf4cd617ec2334

--- dcs-core/src/test/java/com/dcentralized/core/services/common/TestLuceneDocumentIndexService.java
+++ dcs-core/src/test/java/com/dcentralized/core/services/common/TestLuceneDocumentIndexService.java
@@ -96,6 +96,7 @@ import com.dcentralized.core.common.Utils;
 import com.dcentralized.core.common.test.AuthTestUtils;
 import com.dcentralized.core.common.test.ExampleService;
 import com.dcentralized.core.common.test.ExampleService.ExampleImmutableService;
+import com.dcentralized.core.common.test.ExampleService.ExampleNonPersistedService;
 import com.dcentralized.core.common.test.ExampleService.ExampleODLService;
 import com.dcentralized.core.common.test.ExampleService.ExampleServiceState;
 import com.dcentralized.core.common.test.ExampleServiceHost;
@@ -2261,6 +2262,53 @@ public class TestLuceneDocumentIndexService {
             Operation get = Operation.createGet(uri);
             sender.sendAndWaitFailure(get);
         }
+    }
+
+    @Test
+    public void getExpiredNonPersistentServices() throws Throwable {
+        // this test verifies bug fix https://jira.eng.vmware.com/browse/VRXEN-58
+        // (IllegalStateException: Version is non zero but no state was found)
+
+        this.host = VerificationHost.create(0);
+        // set high maintenance interval to prevent cache eviction
+        this.host.setMaintenanceIntervalMicros(TimeUnit.MINUTES.toMicros(5));
+        this.host.start();
+
+        this.host.startFactory(new ExampleNonPersistedService());
+        this.host.waitForServiceAvailable(ExampleNonPersistedService.FACTORY_LINK);
+
+        TestRequestSender sender = this.host.getTestRequestSender();
+
+        // create a set of non-persistent services
+        List<Operation> posts = new ArrayList<>();
+        for (int i = 0; i < this.serviceCount; i++) {
+            ExampleServiceState state = new ExampleServiceState();
+            state.name = "foo-" + i;
+            state.documentSelfLink = state.name;
+
+            posts.add(Operation.createPost(this.host, ExampleNonPersistedService.FACTORY_LINK)
+                    .setBody(state));
+        }
+        List<ExampleServiceState> exampleStates = sender.sendAndWait(posts,
+                ExampleServiceState.class);
+
+        // patch the services with expiration time = now
+        long now = Utils.getNowMicrosUtc();
+        exampleStates.stream().forEach(state -> {
+            state.documentExpirationTimeMicros = now;
+        });
+        List<Operation> patches = new ArrayList<>();
+        exampleStates.stream().forEachOrdered(state -> patches.add(Operation.createPatch(
+                UriUtils.buildUri(this.host, state.documentSelfLink))
+                .setBody(state)));
+        exampleStates = sender.sendAndWait(patches, ExampleServiceState.class);
+
+        // Perform a GET on each of the services.
+        // We expect either a failure or success, but no unhandled exception.
+        List<Operation> gets = new ArrayList<>();
+        exampleStates.stream().forEachOrdered(state -> gets.add(Operation.createGet(
+                UriUtils.buildUri(this.host, state.documentSelfLink))));
+        sender.sendAndWait(gets, false);
     }
 
     @Test
