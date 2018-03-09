68acb0142cac3f63f982b4e7e49fa308ab0033ea
Georgios Chrysanthakopoulos <georgios@dcentralized.com>
2018-03-09 10:13:06 -0800

    apply xenon 2c1d1cc563d9a9d8a64ccc08f06ba26707cefc1a
    https://github.com/vmware/xenon/commit/2c1d1cc563d9a9d8a64ccc08f06ba26707cefc1a

--- dcs-core/src/main/java/com/dcentralized/core/common/ServiceAvailabilityFilter.java
+++ dcs-core/src/main/java/com/dcentralized/core/common/ServiceAvailabilityFilter.java
@@ -114,8 +114,8 @@ public class ServiceAvailabilityFilter implements Filter {
             return FilterReturnCode.SUSPEND_PROCESSING;
         }
 
-        // service is not attached. maybe we should start it on demand.
-
+        // service is not attached or has recently stopped. maybe we should start it on demand.
+        context.setService(null);
         if (ServiceHost.isServiceStop(op)) {
             // local stop - do not start on demand - complete and return
             op.complete();
